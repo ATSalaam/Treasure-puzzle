42849614caf75d8ca621fc25bb3b5e4f18ca0149
ATSalaam <76926662+ATSalaam@users.noreply.github.com>
2021-06-22 09:10:48 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "CAMERA";
+const accessCode3 = "MATTERJS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A M E A R C", 700,80)
+    fill("lightblue")
+    text("Hint: It follows the Player!!", 700,100)
+    
+    fill("white")
+    textSize(15)
+    text("T M A S T E J R", 100,250)
+    fill("lightblue")
+    text("Hint: It gives your game physics!! It has Js", 100,270)
+}
